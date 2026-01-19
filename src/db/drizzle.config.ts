0bf93329f246414619d0fbc17276df0907f6c0b3
vicvans20 <vicvans20@gmail.com>
2026-01-19 16:41:00 -0600

    [4-auth-usuarios-admin] feat: Create migrations for better-auth with drizzle

--- src/db/drizzle.config.ts
+++ src/db/drizzle.config.ts
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-  schema: "./src/db/schema.ts",
+  schema: "./src/db/schema",
   out: "./migrations",
   dialect: "postgresql",
   dbCredentials: {
